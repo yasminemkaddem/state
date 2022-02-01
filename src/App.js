a7daa36b0f467a7274f531aa2499337ce7f7743b
yasmine mkaddem <yasminemkaddem1gmail.com>
2022-02-01 07:40:57 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "yasmine mkaddem",
+      bio: " I am a Junior Web Developer Technologies. I am an accomplished coder and programmer, and I enjoy using my skills to contribute to the exciting technological advances that happen every day at Oswald Tech. I graduated from Institute of Technology in 2019 with a Bachelor's Degree in Software Development. While in school, I earned the 2015 Edmund Gains Award for my exemplary academic performance and leadership skills.",
+      // imgSrc: myImage,
+      profession: "my profession"
+    },
+    show: true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="image 1"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
 
 export default App;
